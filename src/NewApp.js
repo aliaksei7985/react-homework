182b633ec67f8c863cccde7388261ebbda7bae6c
aliaksei7985 <aliakseiriki@gmail.com>
2023-02-22 16:01:56 +0300

    212 lesson

---
+++ src/NewApp.js
@@ -0,0 +1,10 @@
+import './NewApp.css';
+
+export default function NewApp() {
+    return (
+        <div className="newApp">
+            <h1>hi</h1>
+            <div>hi there</div>
+        </div>
+    )
+}
